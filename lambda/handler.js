7858d892a3eaf2103f484ad73d65957bfdf88406
miek <mike.fleming.dev@gmail.com>
2019-11-14 18:40:57 -0600

    adds cors headers, adds index.html

---
+++ lambda/handler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const https = require('https');
+
+const { DARK_SKY_API_KEY } = process.env;
+
+async function getWeather({ queryStringParameters: { lat, lng } }) {
+  try {
+    const { currently } = await get(
+      `https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/${lat},${lng}`
+    );
+
+    return respond(currently, 200);
+  } catch (e) {
+    return respond(e.message, 500);
+  }
+}
+
+function get (url) {
+  return new Promise((resolve, reject) => {
+    let data = '';
+
+    https.get(url, (response) => {
+      response.on('data', chunk => data += chunk);
+      response.on('end', () => resolve(JSON.parse(data)));
+    }).on('error', e => reject(e));
+  });
+}
+
+function respond (data, statusCode) {
+  return {
+    headers: {
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    statusCode,
+    body: JSON.stringify(data)
+  };
+}
+
+exports.main = getWeather;
